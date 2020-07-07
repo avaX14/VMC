401f8461a0966127b257bde0b77f93ff5680a743
Karac Branko <karacbranko@gmail.com>
2020-07-08 00:34:18 +0200

    Updates

---
+++ precache-manifest.b1f9012a33d6c7938d155711bc4e9df8.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e50f42adade7f141c5fbdb8390481cf",
+    "url": "/VMC/index.html"
+  },
+  {
+    "revision": "52b4744996f40e94f61e",
+    "url": "/VMC/static/css/4.48772c93.chunk.css"
+  },
+  {
+    "revision": "73c7d65c91148352ec37",
+    "url": "/VMC/static/css/main.69d3917f.chunk.css"
+  },
+  {
+    "revision": "ecd6964993c610a141c4",
+    "url": "/VMC/static/js/0.8c58f803.chunk.js"
+  },
+  {
+    "revision": "3f51362bcb578140920e",
+    "url": "/VMC/static/js/1.118a2229.chunk.js"
+  },
+  {
+    "revision": "09034e3f3b97da12e37f",
+    "url": "/VMC/static/js/10.de556ce9.chunk.js"
+  },
+  {
+    "revision": "a141edf92a3ca043c1a7",
+    "url": "/VMC/static/js/11.4e629071.chunk.js"
+  },
+  {
+    "revision": "efac9cbc7426039063b3",
+    "url": "/VMC/static/js/12.2c3c2dcd.chunk.js"
+  },
+  {
+    "revision": "4db27736af65e99b407b",
+    "url": "/VMC/static/js/13.5c97357d.chunk.js"
+  },
+  {
+    "revision": "efbfd342e9d2e6f460e3",
+    "url": "/VMC/static/js/14.7f07b8c6.chunk.js"
+  },
+  {
+    "revision": "52b4744996f40e94f61e",
+    "url": "/VMC/static/js/4.794b82e3.chunk.js"
+  },
+  {
+    "revision": "0e01badf6cfffaa4c769",
+    "url": "/VMC/static/js/5.6a093a24.chunk.js"
+  },
+  {
+    "revision": "86e47c9847e7746f9977",
+    "url": "/VMC/static/js/6.a47a152c.chunk.js"
+  },
+  {
+    "revision": "58996f75fab3fe35363e",
+    "url": "/VMC/static/js/7.836a11a9.chunk.js"
+  },
+  {
+    "revision": "ddab1533acf3ad96b136",
+    "url": "/VMC/static/js/8.c3f29add.chunk.js"
+  },
+  {
+    "revision": "d7e2ab8b985298b63fd8",
+    "url": "/VMC/static/js/9.17985b31.chunk.js"
+  },
+  {
+    "revision": "73c7d65c91148352ec37",
+    "url": "/VMC/static/js/main.ba80190d.chunk.js"
+  },
+  {
+    "revision": "ae4297a0c7435f30cb8d",
+    "url": "/VMC/static/js/runtime-main.e15fc6c5.js"
+  },
+  {
+    "revision": "19bc0f34a34387de0ccd082d2f1d37f0",
+    "url": "/VMC/static/media/Map_of_Serbia.19bc0f34.svg"
+  },
+  {
+    "revision": "6660afd421cbd305f0b55f67d188ce21",
+    "url": "/VMC/static/media/PIN.6660afd4.svg"
+  },
+  {
+    "revision": "e1f6663076900d175d92eb640a324bc7",
+    "url": "/VMC/static/media/biljana_pekez.e1f66630.png"
+  },
+  {
+    "revision": "c9ca377bbab8c2efb88485a00ab7fd14",
+    "url": "/VMC/static/media/daniela_zivkovic.c9ca377b.jpeg"
+  },
+  {
+    "revision": "43814393dd5ff1697ed9ba485e05c6ae",
+    "url": "/VMC/static/media/desNoviSad.43814393.png"
+  },
+  {
+    "revision": "4abb2c85c05d1ed5d924a7296e219c57",
+    "url": "/VMC/static/media/facebook.4abb2c85.svg"
+  },
+  {
+    "revision": "b3e0c51835153e850b8b2119db2e52cf",
+    "url": "/VMC/static/media/goal1.b3e0c518.svg"
+  },
+  {
+    "revision": "1fb9fb8d84fbf7856d6b13619337390c",
+    "url": "/VMC/static/media/idea1.1fb9fb8d.svg"
+  },
+  {
+    "revision": "9af18b6e487d4ec994a662a143fea211",
+    "url": "/VMC/static/media/joel-filipe-VuwAfoHpxgs-unsplash 1.9af18b6e.png"
+  },
+  {
+    "revision": "8d3bcda5992a8f61ecbb8a63ba30d73f",
+    "url": "/VMC/static/media/joel-filipe-VuwAfoHpxgs-unsplash 2.8d3bcda5.png"
+  },
+  {
+    "revision": "153495e639a5f200c2d21bda4074c9e5",
+    "url": "/VMC/static/media/linkedin.153495e6.svg"
+  },
+  {
+    "revision": "10ad1abd6491be9ea4f8e11e317e76a9",
+    "url": "/VMC/static/media/logo1.10ad1abd.svg"
+  },
+  {
+    "revision": "69167c9903619c891d9ab85c9b3e4f0c",
+    "url": "/VMC/static/media/milica_jovicic.69167c99.jpeg"
+  },
+  {
+    "revision": "a4b3ab498a9296212a8427956401b3d1",
+    "url": "/VMC/static/media/mirjana_tomin.a4b3ab49.jpeg"
+  },
+  {
+    "revision": "9c34b46918378b4ec56b94cb735990e6",
+    "url": "/VMC/static/media/organizaciona_struktura.9c34b469.svg"
+  },
+  {
+    "revision": "439586d5804dd45d26c6a0f6e43c81a1",
+    "url": "/VMC/static/media/phone1.439586d5.svg"
+  },
+  {
+    "revision": "fee012f2554de0eb778dd44be25d149a",
+    "url": "/VMC/static/media/salesman.fee012f2.svg"
+  },
+  {
+    "revision": "a4dcdff680b0938c7f5857f84afeefe0",
+    "url": "/VMC/static/media/team1.a4dcdff6.svg"
+  },
+  {
+    "revision": "5cdab08999ff4fe364923d36d7301689",
+    "url": "/VMC/static/media/team2.5cdab089.svg"
+  },
+  {
+    "revision": "b32b07d4f398b2b910f5784c510dad87",
+    "url": "/VMC/static/media/training.b32b07d4.svg"
+  },
+  {
+    "revision": "dac4d0342d5801c4216220ec7ea494f8",
+    "url": "/VMC/static/media/twitter.dac4d034.svg"
+  },
+  {
+    "revision": "a542f34644766cbd91d36059c88c72ce",
+    "url": "/VMC/static/media/vladimir_pandurov.a542f346.jpeg"
+  },
+  {
+    "revision": "b5cf18c4df2423678cdc921ff4ef9f23",
+    "url": "/VMC/static/media/youtube.b5cf18c4.svg"
+  },
+  {
+    "revision": "848b80404e66e50e8713191414587ec6",
+    "url": "/VMC/static/media/zoran_pekez.848b8040.png"
+  }
+]);
